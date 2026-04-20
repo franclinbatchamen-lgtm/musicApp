83b6e190c6f60add1f9f47db31cb08994cd71f28
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-04-20 20:53:04 +0200

    added icon, 4th song, 2nd secondary constructor

--- MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var songDisplayTxt = findViewById<TextView>(R.id.musicDisplayID)
+        var songDisplay = ""
+
+
+        var song1 = song("Promises","Maverick City", 2025, 10.46)
+        var song2 = song("KELEBU", "Rema",2025,2.52)
+        var song3 = song("Basquiate", "Asake")
+        var song4 = song("Thank You, God", 3.53 )
+
+        //creating array
+        var songs = arrayOf<song>(song1, song2, song3, song4)
+
+        //for loop to loop through and display items in song array
+        for (song in songs){
+            songDisplay += "${song.DisplaySongs()}\n\n"
+        }
+
+        songDisplayTxt.text = songDisplay
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- MusicApp/app/src/main/java/com/example/musicapp/song.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/song.kt
@@ -2,27 +2,20 @@ package com.example.musicapp
 
 import android.util.Log
 
-
-class song (songName: String, songArtist: String, songYear: Int, songDuration: String) {
+class song (songName: String, songArtist: String, songYear: Int, songDuration: Double) {
 
     //adding variables for the object's attributes
     var name: String = ""
     var artist: String = ""
     var year: Int = 0
-    var duration: String = ""
+    var duration: Double = 0.0
 
-    //secondary constructor
-    constructor(songName: String, songArtist: String) : this(songName, songArtist, 0, "") {
-
+    constructor(songName: String, songDuration: Double) : this(songName, "", 0, songDuration ) {
     }
 
-
-
-
-
-
-
-
+    //secondary constructor just in case the user doesn't input a year or duration
+    constructor(songName: String, songArtist: String) : this(songName, songArtist, 0, 0.0) {
+    }
 
     // initialising the variables for attribute for the song objects
     init{
@@ -33,7 +26,7 @@ class song (songName: String, songArtist: String, songYear: Int, songDuration: S
     }
 
 
-    // funtion to play song
+    // function to play song
     fun Play() {
         Log.v("Song", "$name is playing")
     }
@@ -52,6 +45,10 @@ class song (songName: String, songArtist: String, songYear: Int, songDuration: S
     fun Favorite(): Boolean {
         Log.v("Song", "$name is a favorite")
         return true
+    }
+    // add function to display song attributes
+    fun DisplaySongs() : String {
+        return "$name, $artist, ($duration)"
     }
 
 }
